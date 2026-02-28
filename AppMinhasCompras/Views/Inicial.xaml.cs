c2e091c57a5f6441a8b43acb20b2bc42ba89bc8c
PC1 <PC1@DESKTOP-70V109E>
2026-02-28 17:43:52 -0300

    Adicionar arquivos de projeto.

---
+++ AppMinhasCompras/Views/Inicial.xaml.cs
@@ -0,0 +1,17 @@
+namespace AppMinhasCompras.Views;
+
+public partial class Inicial : ContentPage
+{
+    public Inicial()
+    {
+        InitializeComponent();
+    }
+    private async void OnCadastrarClicked(object sender, EventArgs e) { }
+    
+
+    private async void OnListarClicked(object sender, EventArgs e) { }
+    
+
+    private async void OnEditarClicked(object sender, EventArgs e) { }
+    
+}
